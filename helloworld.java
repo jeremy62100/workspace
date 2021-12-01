505a50339a49c1531b28c2cd473db5572c1a750a
jeremy62100 <jeremy.delannoy65@gmail.com>
2021-12-01 13:21:02 +0100

    Mon Premier commit

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+
+	public static void main(String[] args) {
+		// Va imprimé "Bonjour tout le monde dans le terminal" -- Print "Bonjour tout le monde to the terminal"
+		System.out.println("Bonjour tout le monde")
+	}
+}
